335eeefbe5844d2cc33c700bad53838502419cda
Faddal Ibrahim <faddalibrahim@gmail.com>
2022-11-25 01:20:44 +0000

    fix color mapping bug

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -15,19 +15,6 @@ export default function Settings({ jobs, setJobs }) {
   const LABEL = "label";
   const COLOR_INDEX = "colorIndex";
 
-  // const COLORS = [
-  //   ["rgba(255, 99, 132,0.2)", "rgba(255, 99, 132,1)"],
-  //   ["rgba(54, 162, 235,0.2)", "rgba(54, 162, 235,1)"],
-  //   ["rgba(75, 192, 192,0.2)", "rgba(75, 192, 192,1)"],
-  //   ["rgb(242 232 109 / 30%)", "rgb(242 232 109 / 100%)"],
-  //   ["rgba(160,99,255,0.2)", "rgba(160,99,255,1)"],
-  //   ["rgba(218,191,255,0.2)", "rgba(218,191,255,1)"],
-  //   ["rgb(127 222 255 / 30%)", "rgb(127 222 255 / 100%)"],
-  //   ["rgb(198 236 174 / 30%)", "rgb(198 236 174 / 100%)"],
-  //   ["rgb(245 26 164 / 30%)", "rgb(245 26 164 / 100%)"],
-  //   ["rgb(39 193 75 / 30%)", "rgb(39 193 75 / 100%)"],
-  // ];
-
   const handleJobCountChange = (e) => {
     let currCount = jobs.length;
     let newJobCount = e.target.value;
@@ -41,7 +28,7 @@ export default function Settings({ jobs, setJobs }) {
           [RUN_TIME]: "",
           [ANIMATION_DELAY]: "",
           [LABEL]: indexToLetter(j),
-          [COLOR_INDEX]: jobs.length,
+          [COLOR_INDEX]: newJobs.length,
         });
     }
 
